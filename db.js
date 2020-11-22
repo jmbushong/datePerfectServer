578f73e13b95cd900786cace1aa9ad4aaca2fdc7
Jessica Bushong <jessica.bushong08@gmail.com>
2020-11-22 15:18:50 -0500

    updated db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@
 const Sequelize = require('sequelize');
 
 //create a new instance of sequelize, connecting us to database
-const database = new Sequelize (process.env.NAME, 'postgres', process.env.PASS, {host:'localhost',
-dialect:'postgres'});
+// const database = new Sequelize (process.env.NAME, 'postgres', process.env.PASS, {host:'localhost',
+// dialect:'postgres'});
 
-// const database = new Sequelize (process.env.DATABASE_URL, {
-//     dialect:'postgres'});
+const database = new Sequelize (process.env.DATABASE_URL, {
+    dialect:'postgres'});
 //authenticate that the username and password match, then log into the database
 
 database.authenticate()
